814862cd84a8fe0bc58a5529c23d374e61c191b2
ROGITHB <rb19042000@gmail.com>
2024-10-22 16:26:21 +0530

    Added init

--- index.js
+++ index.js
@@ -1,86 +1,19 @@
 const express = require('express');
-const recipes = require("./recipes.json");
-const RecipesController = require("./controller/Recipes.controller");
-const {createDbconnect} = require('./db');
+const bodyParser = require('body-parser');
+const connectDB = require('./db');
+const recipeRoutes = require('./routes/recipesRoutes');
+const dotenv = require('dotenv');
 
-// CREATE DB CONNECTION
-createDbconnect();
+dotenv.config();
 
-// STEP 1: Create a API SERVER
-const app = Express()
-// PARSING INCOMING REQUEST BODY AS JSON
-app.use(Express.json());
+const app = express();
+connectDB();
 
-//SERVING STATIC FILES
-app.use(Express.static('public'));
-// INJECT ROUTERS
-app.use("/recipes", RecipesController)
-// ROUTE 1: /
-app.get("/", function (request, response) {
-  // return response.send("/ - Request received")
-  return response.status(200).json({
-   message: "Hello Folks",
-   success: true
-  });
-});
+app.use(bodyParser.json());
+app.use('/api/recipes', recipeRoutes);
 
-// ROUTE 2: 
-// PATH = /recipes
-// METHOD = GET
-app.get("/recipes", function (request, response) {
-  let result = [];
-  const { limit, page } = request.query;
-  if(limit && page) {
-      const start = Number(limit) * (Number(page) - 1) ;
-      const end = Number(limit) * Number(page);
-      result = recipes.data.slice(start, end);
-  } else {
-      result = products.data;
-  }
-  // return response.send("/ - Request received")
-  return response.status(200).json({
-   message: "recipes fetched successfully",
-   success: true,
-   data: result
-  });
-});
+const PORT = process.env.PORT || 3000;
 
-// ROUTE 2: 
-// PATH = /recipes/{recipesId}
-// METHOD = GET
-app.get("/recipes/:recipesId", function (request, response) {
-  // return response.send("/ - Request received"))
-  const matchedrecipes = recipes.data.find((_recipes) => _recipes.id === Number(request.params.recipesId));
-  if (!matchedProduct) {
-      return response.status(404).json({
-          message: "No recipes found",
-          success: false
-         });    
-  } else {
-      return response.status(200).json({
-          message: "recipes fetched successfully",
-          success: true,
-          data: matchedrecipes
-         });
-  }
-});
-
-
-// ROUTE 4:
-// PATH = /recipes
-// METHOD = POST
-API_SERVER.post("/recipes/create", function (request, response) {
-  console.log("HERE", request.body);
-  return response.json({
-      message: "recipes created successfully!",
-      success: true
-  });
-});
-
-
-
-//step2:START AND LISTEN INCOMING REQUEST TO THIS SERVER
-app.listen(process.env.PORT, process.env.HOSTNAME, function () {
-    console.log("Server started");
-    console.log(`http://${process.env.HOSTNAME}:${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
